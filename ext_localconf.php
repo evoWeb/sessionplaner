35429c93aa298dbe5b39816033f50943999eb389
Richard Haeser <richard@maxserv.com>
2018-12-12 15:24:53 +0100

    [TASK] Fixed tsconfig markup

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,7 +14,7 @@ call_user_func(function () {
      * Default PageTS
      */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sessionplaner/Configuration/PageTS/ModWizards.ts">'
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sessionplaner/Configuration/PageTS/ModWizards.tsconfig">'
     );
 
     /**
