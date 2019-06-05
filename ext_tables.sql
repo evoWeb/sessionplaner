3507bbfaf10f4139f15bc7b645e06dd89224231c
Benjamin Kott <benjamin.kott@outlook.com>
2019-06-05 10:09:00 +0200

    [TASK] Add more indention headroom to sql definition

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,21 +3,21 @@
 #
 CREATE TABLE tx_sessionplaner_domain_model_day
 (
-    uid     int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-    pid     int(11) unsigned DEFAULT '0' NOT NULL,
-    tstamp  int(11) unsigned DEFAULT '0' NOT NULL,
-    crdate  int(11) unsigned DEFAULT '0' NOT NULL,
-    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden  tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    uid             int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid             int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp          int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate          int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted         tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden          tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
-    name    varchar(255) DEFAULT '' NOT NULL,
-    date    varchar(255) DEFAULT '' NOT NULL,
-    rooms   int(11) unsigned DEFAULT '0' NOT NULL,
-    slots   int(11) unsigned DEFAULT '0' NOT NULL,
+    name            varchar(255) DEFAULT '' NOT NULL,
+    date            varchar(255) DEFAULT '' NOT NULL,
+    rooms           int(11) unsigned DEFAULT '0' NOT NULL,
+    slots           int(11) unsigned DEFAULT '0' NOT NULL,
 
     PRIMARY KEY (uid),
-    KEY     parent (pid),
-    KEY     enable (deleted,hidden)
+    KEY             parent (pid),
+    KEY             enable (deleted,hidden)
 );
 
 #
@@ -25,25 +25,25 @@ CREATE TABLE tx_sessionplaner_domain_model_day
 #
 CREATE TABLE tx_sessionplaner_domain_model_room
 (
-    uid      int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-    pid      int(11) unsigned DEFAULT '0' NOT NULL,
-    tstamp   int(11) unsigned DEFAULT '0' NOT NULL,
-    crdate   int(11) unsigned DEFAULT '0' NOT NULL,
-    sorting  int(11) unsigned DEFAULT '0' NOT NULL,
-    deleted  tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden   tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    uid             int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid             int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp          int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate          int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting         int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted         tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden          tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
-    name     varchar(255) DEFAULT '' NOT NULL,
-    logo     int(11) unsigned DEFAULT '0' NOT NULL,
-    seats    int(11) unsigned DEFAULT '0' NOT NULL,
+    name            varchar(255) DEFAULT '' NOT NULL,
+    logo            int(11) unsigned DEFAULT '0' NOT NULL,
+    seats           int(11) unsigned DEFAULT '0' NOT NULL,
 
-    days     int(11) unsigned DEFAULT '0' NOT NULL,
-    slots    int(11) unsigned DEFAULT '0' NOT NULL,
-    sessions int(11) unsigned DEFAULT '0' NOT NULL,
+    days            int(11) unsigned DEFAULT '0' NOT NULL,
+    slots           int(11) unsigned DEFAULT '0' NOT NULL,
+    sessions        int(11) unsigned DEFAULT '0' NOT NULL,
 
     PRIMARY KEY (uid),
-    KEY      parent (pid),
-    KEY      enable (deleted,hidden)
+    KEY             parent (pid),
+    KEY             enable (deleted,hidden)
 );
 
 #
@@ -51,25 +51,25 @@ CREATE TABLE tx_sessionplaner_domain_model_room
 #
 CREATE TABLE tx_sessionplaner_domain_model_slot
 (
-    uid            int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-    pid            int(11) unsigned DEFAULT '0' NOT NULL,
-    tstamp         int(11) unsigned DEFAULT '0' NOT NULL,
-    crdate         int(11) unsigned DEFAULT '0' NOT NULL,
-    deleted        tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden         tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    uid             int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid             int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp          int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate          int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted         tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden          tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
-    day            int(11) unsigned DEFAULT '0' NOT NULL,
-    start          int(11) DEFAULT '0' NOT NULL,
-    duration       int(11) unsigned DEFAULT '45' NOT NULL,
-    break          tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    no_break_after tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    day             int(11) unsigned DEFAULT '0' NOT NULL,
+    start           int(11) DEFAULT '0' NOT NULL,
+    duration        int(11) unsigned DEFAULT '45' NOT NULL,
+    break           tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    no_break_after  tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
-    rooms          int(11) unsigned DEFAULT '0' NOT NULL,
-    sessions       int(11) unsigned DEFAULT '0' NOT NULL,
+    rooms           int(11) unsigned DEFAULT '0' NOT NULL,
+    sessions        int(11) unsigned DEFAULT '0' NOT NULL,
 
     PRIMARY KEY (uid),
-    KEY            parent (pid),
-    KEY            enable (deleted,hidden)
+    KEY             parent (pid),
+    KEY             enable (deleted,hidden)
 );
 
 #
@@ -77,34 +77,34 @@ CREATE TABLE tx_sessionplaner_domain_model_slot
 #
 CREATE TABLE tx_sessionplaner_domain_model_session
 (
-    uid         int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-    pid         int(11) unsigned DEFAULT '0' NOT NULL,
-    tstamp      int(11) unsigned DEFAULT '0' NOT NULL,
-    crdate      int(11) unsigned DEFAULT '0' NOT NULL,
-    deleted     tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden      tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    uid             int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid             int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp          int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate          int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted         tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden          tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
     # references
-    topic       varchar (255) DEFAULT '' NOT NULL,
-    speaker     varchar(255) DEFAULT '' NOT NULL,
-    twitter     varchar(255) DEFAULT '' NOT NULL,
-    attendees   int(11) unsigned DEFAULT '0' NOT NULL,
-    suggestion  tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    social      tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    donotlink   tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    type        int(11) unsigned DEFAULT '0' NOT NULL,
-    level       int(11) unsigned DEFAULT '0' NOT NULL,
-    description text,
-    documents   int(11) unsigned DEFAULT '0' NOT NULL,
+    topic           varchar (255) DEFAULT '' NOT NULL,
+    speaker         varchar(255) DEFAULT '' NOT NULL,
+    twitter         varchar(255) DEFAULT '' NOT NULL,
+    attendees       int(11) unsigned DEFAULT '0' NOT NULL,
+    suggestion      tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    social          tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    donotlink       tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    type            int(11) unsigned DEFAULT '0' NOT NULL,
+    level           int(11) unsigned DEFAULT '0' NOT NULL,
+    description     text,
+    documents       int(11) unsigned DEFAULT '0' NOT NULL,
 
-    day         int(11) unsigned DEFAULT '0' NOT NULL,
-    room        int(11) unsigned DEFAULT '0' NOT NULL,
-    slot        int(11) unsigned DEFAULT '0' NOT NULL,
-    tags        int(11) unsigned DEFAULT '0' NOT NULL,
+    day             int(11) unsigned DEFAULT '0' NOT NULL,
+    room            int(11) unsigned DEFAULT '0' NOT NULL,
+    slot            int(11) unsigned DEFAULT '0' NOT NULL,
+    tags            int(11) unsigned DEFAULT '0' NOT NULL,
 
     PRIMARY KEY (uid),
-    KEY         parent (pid),
-    KEY         enable (deleted,hidden)
+    KEY             parent (pid),
+    KEY             enable (deleted,hidden)
 );
 
 #
@@ -112,19 +112,19 @@ CREATE TABLE tx_sessionplaner_domain_model_session
 #
 CREATE TABLE tx_sessionplaner_domain_model_tag
 (
-    uid      int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-    pid      int(11) unsigned DEFAULT '0' NOT NULL,
-    tstamp   int(11) unsigned DEFAULT '0' NOT NULL,
-    crdate   int(11) unsigned DEFAULT '0' NOT NULL,
-    deleted  tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden   tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    uid             int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid             int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp          int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate          int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted         tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden          tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
-    label    varchar(255) DEFAULT '' NOT NULL,
-    sessions int(11) unsigned DEFAULT '0' NOT NULL,
+    label           varchar(255) DEFAULT '' NOT NULL,
+    sessions        int(11) unsigned DEFAULT '0' NOT NULL,
 
     PRIMARY KEY (uid),
-    KEY      parent (pid),
-    KEY      enable (deleted,hidden)
+    KEY             parent (pid),
+    KEY             enable (deleted,hidden)
 );
 
 #
