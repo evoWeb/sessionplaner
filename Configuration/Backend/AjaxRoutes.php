0fa5493269a4016c9eede26d9b1326d00cb8da26
Benjamin Kott <benjamin.kott@outlook.com>
2023-06-27 13:34:58 +0200

    [TASK] Use imports

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -7,20 +7,22 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use Evoweb\Sessionplaner\Controller\AjaxController;
+
 return [
     // Dispatch the create action
     'evoweb_sessionplaner_create' => [
         'path' => '/evoweb/sessionplaner/create',
-        'target' => \Evoweb\Sessionplaner\Controller\AjaxController::class . '::createSessionAction'
+        'target' => AjaxController::class . '::createSessionAction'
     ],
     // Dispatch the update action
     'evoweb_sessionplaner_update' => [
         'path' => '/evoweb/sessionplaner/update',
-        'target' => \Evoweb\Sessionplaner\Controller\AjaxController::class . '::updateSessionAction'
+        'target' => AjaxController::class . '::updateSessionAction'
     ],
     // Dispatch the delete action
     'evoweb_sessionplaner_delete' => [
         'path' => '/evoweb/sessionplaner/delete',
-        'target' => \Evoweb\Sessionplaner\Controller\AjaxController::class . '::deleteSessionAction'
+        'target' => AjaxController::class . '::deleteSessionAction'
     ]
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,93 +7,68 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use Evoweb\Sessionplaner\Controller\SessionController;
+use Evoweb\Sessionplaner\Updates\SessionPathSegmentUpdate;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 defined('TYPO3') or die('Access denied.');
 
-call_user_func(function () {
-    // Register "sessionplannervh" as global fluid namespace
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['sessionplanervh'][] =
-        'Evoweb\\Sessionplaner\\ViewHelpers';
+// PageTS
+ExtensionManagementUtility::addPageTSConfig('@import \'EXT:sessionplaner/Configuration/PageTS/ModWizards.tsconfig\'');
 
-    /**
-     * Add default PageTsConfig
-     */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '@import \'EXT:sessionplaner/Configuration/PageTS/ModWizards.tsconfig\''
-    );
+// Register "sessionplannervh" as global fluid namespace
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['sessionplanervh'][] = 'Evoweb\\Sessionplaner\\ViewHelpers';
 
-    /**
-     * Register Update Wizards
-     */
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update'][
-        \Evoweb\Sessionplaner\Updates\SessionPathSegmentUpdate::class
-    ] = \Evoweb\Sessionplaner\Updates\SessionPathSegmentUpdate::class;
-
-    /**
-     * Configure Session Frontend Plugin
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Sessionplaner',
-        'Session',
-        [
-            \Evoweb\Sessionplaner\Controller\SessionController::class => 'list, show',
-        ]
-    );
-
-    /**
-     * Configure Sessionplan Frontend Plugin
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Sessionplaner',
-        'Sessionplan',
-        [
-            \Evoweb\Sessionplaner\Controller\SessionplanController::class => 'display',
-        ]
-    );
-
-    /**
-     * Configure Speaker Frontend Plugin
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Sessionplaner',
-        'Speaker',
-        [
-            \Evoweb\Sessionplaner\Controller\SpeakerController::class => 'list, show',
-        ]
-    );
-
-    /**
-     * Configure Suggest Frontend Plugin
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Sessionplaner',
-        'Suggest',
-        [
-            \Evoweb\Sessionplaner\Controller\SuggestController::class => 'form',
-        ],
-        [
-            \Evoweb\Sessionplaner\Controller\SuggestController::class => 'form',
-        ]
-    );
-
-    /**
-     * Register Title Provider
-     */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        trim(
-            '
-    config.pageTitleProviders {
-        event {
-            provider = Evoweb\Sessionplaner\TitleTagProvider\EventTitleTagProvider
-            before = seo
-            after = altPageTitle
-        }
-        speaker {
-            provider = Evoweb\Sessionplaner\TitleTagProvider\SpeakerTitleTagProvider
-            before = seo
-            after = altPageTitle
-        }
+// Register Title Provider
+ExtensionManagementUtility::addTypoScriptSetup(trim('
+config.pageTitleProviders {
+    event {
+        provider = Evoweb\Sessionplaner\TitleTagProvider\EventTitleTagProvider
+        before = seo
+        after = altPageTitle
     }
-'
-        )
-    );
-});
+    speaker {
+        provider = Evoweb\Sessionplaner\TitleTagProvider\SpeakerTitleTagProvider
+        before = seo
+        after = altPageTitle
+    }
+}
+'));
+
+// Plugins
+ExtensionUtility::configurePlugin(
+    'Sessionplaner',
+    'Session',
+    [
+        SessionController::class => 'list, show',
+    ]
+);
+ExtensionUtility::configurePlugin(
+    'Sessionplaner',
+    'Sessionplan',
+    [
+        SessionplanController::class => 'display',
+    ]
+);
+ExtensionUtility::configurePlugin(
+    'Sessionplaner',
+    'Speaker',
+    [
+        SpeakerController::class => 'list, show',
+    ]
+);
+ExtensionUtility::configurePlugin(
+    'Sessionplaner',
+    'Suggest',
+    [
+        SuggestController::class => 'form',
+    ],
+    [
+        SuggestController::class => 'form',
+    ]
+);
+
+// Upgrades
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update'][SessionPathSegmentUpdate::class]
+    = SessionPathSegmentUpdate::class;
