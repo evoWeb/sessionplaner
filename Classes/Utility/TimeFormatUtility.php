0eea861ae2e799b4512805b8b2ddc92cc9e33ed3
Marvin Buchmann <marvin.buchmann@typo3.com>
2025-07-03 13:14:46 +0200

    Explicitly cast to int, prevent warning

--- Classes/Utility/TimeFormatUtility.php
+++ Classes/Utility/TimeFormatUtility.php
@@ -15,9 +15,8 @@ class TimeFormatUtility
 {
     public static function getFormattedTime(int $value): string
     {
-        $value = (int)$value;
         $hours = (string)floor($value / 3600);
-        $minutes = (string)floor(($value / 60) % 60);
+        $minutes = (string)floor((int)($value / 60) % 60);
 
         return str_pad($hours, 2, '0', STR_PAD_LEFT) . ':' . str_pad($minutes, 2, '0', STR_PAD_LEFT);
     }
