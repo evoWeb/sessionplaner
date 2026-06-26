bd54e66f6e7e147bfe2cd26d8f17061591b0e3a3
Sebastian Fischer <typo3@evoweb.de>
2026-06-26 18:34:57 +0200

    [TASK] Update test infrastructure

--- Build/php-cs-fixer/config.php
+++ Build/php-cs-fixer/config.php
@@ -2,60 +2,71 @@
 
 declare(strict_types=1);
 
-/**
- * This file represents the configuration for Code Sniffing PSR-2-related
- * automatic checks of coding guidelines
- * Install @fabpot's great php-cs-fixer tool via
+/*
+ * This file is copied from the TYPO3 CMS project.
  *
- *  $ composer global require friendsofphp/php-cs-fixer
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * And then simply run
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  $ php-cs-fixer fix
- *
- * For more information read:
- *  http://www.php-fig.org/psr/psr-2/
- *  http://cs.sensiolabs.org
+ * The TYPO3 project - inspiring people to share!
  */
 
+/**
+ * This file represents the configuration for Code Sniffing PER-related
+ * automatic checks of coding guidelines.
+ *
+ * Run it using runTests.sh, see 'runTests.sh -h' for more options.
+ *
+ * Fix entire extension:
+ * > Build/Scripts/additionalTests.sh -p 8.3 -s composerInstallPackage -q "typo3/cms-core:[dev-main,13...]"
+ * > Build/Scripts/runTests.sh -s cgl
+ *
+ * Fix your current patch:
+ * > Build/Scripts/runTests.sh -s cglGit
+ */
 if (PHP_SAPI !== 'cli') {
     die('This script supports command line usage only. Please check your command.');
 }
 
-$header = <<<EOF
-This file is part of the package evoweb/sessionplaner.
-
-For the full copyright and license information, please read the
-LICENSE file that was distributed with this source code.
-EOF;
-
+// Return a Code Sniffing configuration using
+// all sniffers needed for PER
+// and additionally:
+//  - Remove leading slashes in use clauses.
+//  - PHP single-line arrays should not have trailing comma.
+//  - Single-line whitespace before closing semicolon are prohibited.
+//  - Remove unused use statements in the PHP source code
+//  - Ensure Concatenation to have at least one whitespace around
+//  - Remove trailing whitespace at the end of blank lines.
 return (new \PhpCsFixer\Config())
-    ->setRiskyAllowed(true)
+    ->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setFinder(
-        \PhpCsFixer\Finder::create()
-            ->in(__DIR__ . '/../../')
-            ->exclude('bin')
-            ->exclude('Build')
-            ->exclude('config')
-            ->exclude('var')
+        (new PhpCsFixer\Finder())
+            ->ignoreVCSIgnored(true)
+            ->in([
+                __DIR__ . '/../../',
+            ])
+            ->exclude([
+                '.cache',
+                'bin',
+                'Build',
+                'typo3temp',
+                'public',
+                'var',
+                'vendor',
+            ])
     )
+    ->setRiskyAllowed(true)
     ->setRules([
         '@DoctrineAnnotation' => true,
-        // @todo: Switch to @PER-CS2x0 once php-cs-fixer's todo list is
-        // done: https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/7247
-        '@PER-CS1x0' => true,
-        'array_indentation' => true,
-        'array_syntax' => ['syntax' => 'short'],
+        '@PER-CS3x0' => true,
+        // Override PER-CS3x0 default (single) to keep no space after cast operators
         'cast_spaces' => ['space' => 'none'],
-        // @todo: Can be dropped once we enable @PER-CS2x0
-        'concat_space' => ['spacing' => 'one'],
-        'declare_equal_normalize' => ['space' => 'none'],
         'declare_parentheses' => true,
         'dir_constant' => true,
-        // @todo: Can be dropped once we enable @PER-CS2x0
-        'function_declaration' => [
-            'closure_fn_spacing' => 'none',
-        ],
         'function_to_constant' => [
             'functions' => [
                 'get_called_class',
@@ -64,7 +75,7 @@ return (new \PhpCsFixer\Config())
                 'php_sapi_name',
                 'phpversion',
                 'pi',
-            ]
+            ],
         ],
         'type_declaration_spaces' => true,
         'global_namespace_import' => [
@@ -73,11 +84,14 @@ return (new \PhpCsFixer\Config())
             'import_functions' => false,
         ],
         'list_syntax' => ['syntax' => 'short'],
-        // @todo: Can be dropped once we enable @PER-CS2x0
-        'method_argument_space' => true,
         'modernize_strpos' => true,
         'modernize_types_casting' => true,
         'native_function_casing' => true,
+        'native_function_invocation' => [
+            'include' => [],
+            'scope' => 'all',
+            'strict' => true,
+        ],
         'no_alias_functions' => true,
         'no_blank_lines_after_phpdoc' => true,
         'no_empty_phpdoc' => true,
@@ -93,6 +107,7 @@ return (new \PhpCsFixer\Config())
         'no_unused_imports' => true,
         'no_useless_else' => true,
         'no_useless_nullsafe_operator' => true,
+        // Override PER-CS3x0 default (union) to keep ?Type shorthand syntax
         'nullable_type_declaration' => [
             'syntax' => 'question_mark',
         ],
@@ -101,7 +116,8 @@ return (new \PhpCsFixer\Config())
         'ordered_imports' => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
         'php_unit_construct' => ['assertions' => ['assertEquals', 'assertSame', 'assertNotEquals', 'assertNotSame']],
         'php_unit_mock_short_will_return' => true,
-        'php_unit_test_case_static_method_calls' => ['call_type' => 'self',
+        'php_unit_test_case_static_method_calls' => [
+            'call_type' => 'self',
             'methods' => [
                 'any' => 'this',
                 'atLeast' => 'this',
@@ -127,12 +143,8 @@ return (new \PhpCsFixer\Config())
         'phpdoc_types' => true,
         'phpdoc_types_order' => ['null_adjustment' => 'always_last', 'sort_algorithm' => 'none'],
         'protected_to_private' => true,
-        'return_type_declaration' => ['space_before' => 'none'],
         'single_quote' => true,
-        'single_space_around_construct' => true,
         'single_line_comment_style' => ['comment_types' => ['hash']],
-        // @todo: Can be dropped once we enable @PER-CS2x0
-        'single_line_empty_body' => true,
         'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
